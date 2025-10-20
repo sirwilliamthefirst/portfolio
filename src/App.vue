4ab9ae1b6627d5208ba5d95f34fa996279b54161
sirwilliamthefirst <30707549+sirwilliamthefirst@users.noreply.github.com>
2025-10-20 08:09:46 -0400

    added offset for mouse click

--- src/App.vue
+++ src/App.vue
@@ -11,42 +11,8 @@ import Pond from './components/Pond.vue'
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
 .pond_container {
-  min-height: 50vh; /* or however tall you want */
-  min-width: 50vw;
-}
-body,
-html {
-  margin: 0;
-  padding: 0;
-  height: 100%;
-  background: #111;
-  overflow: hidden;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+  width: 50vw;
+  height: 50vh;
 }
 </style>
--- src/components/Pond.vue
+++ src/components/Pond.vue
@@ -3,8 +3,11 @@ import { ref } from 'vue'
 import Ripple from './Ripple.vue'
 const ripples = ref<{ x: number; y: number }[]>([])
 function createRipple(event: MouseEvent) {
-  ripples.value.push({ x: event.clientX, y: event.clientY })
-  console.log(event.clientX, event.clientY)
+  const target = event.currentTarget as HTMLElement
+  const rect = target.getBoundingClientRect()
+  const x = event.clientX - rect.left
+  const y = event.clientY - rect.top
+  ripples.value.push({ x, y })
 }
 </script>
 
@@ -18,7 +21,6 @@ function createRipple(event: MouseEvent) {
 
 <style>
 .pond {
-  /*background: linear-gradient(to bottom, lightblue, rgb(72, 72, 124));*/
   background: radial-gradient(circle, lightblue, rgb(72, 72, 124));
   width: 100%;
   height: 100%;

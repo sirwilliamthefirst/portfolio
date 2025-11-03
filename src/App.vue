cdeb6149ee92c25c463b3a1aed51f391fb7b220c
sirwilliamthefirst <30707549+sirwilliamthefirst@users.noreply.github.com>
2025-11-02 20:39:44 -0500

    added pop out effect for title

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@ import Pond from './components/Pond.vue'
 </script>
 
 <template>
+  <div class="pond_title"><h1>Poetry Pond</h1></div>
   <div class="pond_container">
     <Pond></Pond>
   </div>
@@ -16,5 +17,37 @@ import Pond from './components/Pond.vue'
   height: 100vh;
   margin: 0 auto; /* centers horizontally */
   overflow: hidden;
+}
+
+@font-face {
+  font-family: 'cloudy_font';
+  src: url('./assets/cotton-cloud.regular.ttf') format('truetype');
+}
+
+@keyframes title_pop {
+  0% {
+    opacity: 0;
+    transform: scale(0.5);
+  }
+  70% {
+    opacity: 1;
+    transform: scale(1.2); /* Overshoot - bigger than final */
+  }
+  100% {
+    opacity: 1;
+    transform: scale(1); /* Settle to normal size */
+  }
+}
+.pond_title {
+  position: fixed;
+  color: white;
+  width: 100%;
+  text-align: center;
+  top: 2%;
+  z-index: 1;
+  font-family: 'cloudy_font';
+  pointer-events: none;
+  opacity: 0;
+  animation: title_pop 0.4s linear 2 forwards;
 }
 </style>
